7a40a222dcb2197a90461456a4c740b3300161bf
Arjun Rai Mahendra <arjunraim@gmail.com>
2023-09-25 15:30:45 +0530

    style(lines): add new line in imports

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask_migrate import Migrate
 from db import db
 import models
 
+
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
